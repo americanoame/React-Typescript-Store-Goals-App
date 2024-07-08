15350f71c7f1430fc50ec9fd93d53cca50ffb0cd
Americano.Dev <andrenations@gmail.com>
2024-07-08 15:39:22 -0400

    add delete functionality in GoalAppList component with onDeleteGoal handler

--- frontend/src/components/GoalAppList.tsx
+++ frontend/src/components/GoalAppList.tsx
@@ -3,14 +3,15 @@ import { Goal } from "../App";
 
 type GoalListProps = {
   goals: Goal[];
+  onDeleteGoal: (id: number) => void;
 };
 
-export default function GoalAppList({goals}: GoalListProps) {
+export default function GoalAppList({goals, onDeleteGoal}: GoalListProps) {
   return (
     <ul>
       {goals.map((goal) => (
         <li key={goal.id}>
-          <GoalApp title={goal.title}>
+          <GoalApp title={goal.title} onDelete={() => onDeleteGoal(goal.id)}>
             <p>{goal.description}</p>
           </GoalApp>
         </li>
