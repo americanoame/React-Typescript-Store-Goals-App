a5cb5caaf2bf13a3c2597eebea0c2266d0aab259
Americano.Dev <andrenations@gmail.com>
2024-07-07 20:17:13 -0400

    Add Header component to App with goals image and title

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,13 @@
 import GoalApp from './components/GoalApp';
+import goalsImg from './assets/goals.jpg';
+import Header from './components/Header';
 
 export default function App() {
   return (
     <main>
+      <Header image={{ src: goalsImg, alt: "A list of goals" }}>
+        <h1>Goals</h1>
+      </Header>
       <GoalApp title="Goal App">
         <p>A simple goal tracking app that allows user to create, update, and delete goals.</p> 
       </GoalApp>
