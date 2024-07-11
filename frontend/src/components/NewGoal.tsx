3b2eff25c0a1ec488ce06ddd33cd04abf60e42f0
Americano.Dev <andrenations@gmail.com>
2024-07-10 21:27:07 -0400

    Add useRef hooks for goal and summary inputs; implement handleSubmit function in NewGoal component

--- frontend/src/components/NewGoal.tsx
+++ frontend/src/components/NewGoal.tsx
@@ -1,17 +1,33 @@
-export default function NewGoal() {
-  function handleSubmit(event: React.FormEvent) {
+import { useRef, type FormEvent } from "react";
+
+type newGoalProps = {
+  onAddGoal: (goal: string, summary: string) => void;
+};
+
+export default function NewGoal({onAddGoal}: newGoalProps) {
+  const goal = useRef<HTMLInputElement>(null);
+  const summary = useRef<HTMLInputElement>(null);
+
+
+  function handleSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
+
+    const enteredGoal = goal.current!.value;
+    const enteredSummary = summary.current!.value;
+
+    event.currentTarget.reset();
+    onAddGoal(enteredGoal, enteredSummary);
   }
 
   return (
     <form onSubmit={handleSubmit}>
       <p>
         <label htmlFor="goal">Your Goal</label>
-        <input type="text" id="goal" />
+        <input type="text" id="goal" ref={goal}  />
       </p>
       <p>
         <label htmlFor="summary">Description</label>
-        <input type="text" id="summary" />
+        <input type="text" id="summary" ref={summary} />
       </p>
       <p>
         <button>Add Goal</button>
