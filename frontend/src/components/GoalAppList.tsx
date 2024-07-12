5a21229996ab283e0f8ea6583d00b25d3b493f35
Americano.Dev <andrenations@gmail.com>
2024-07-12 17:08:22 -0400

    Add GoalAppList component and display warning box when goals are 3 or more

--- frontend/src/components/GoalAppList.tsx
+++ frontend/src/components/GoalAppList.tsx
@@ -1,21 +1,44 @@
-import GoalApp from "./GoalApp";
-import { Goal } from "../App";
+import { type ReactNode } from 'react';
+import GoalApp from './GoalApp';
+import { Goal } from '../App';
+import InfoBox from './InfoBox';
 
 type GoalListProps = {
   goals: Goal[];
   onDeleteGoal: (id: number) => void;
 };
 
-export default function GoalAppList({goals, onDeleteGoal}: GoalListProps) {
+export default function GoalAppList({ goals, onDeleteGoal }: GoalListProps) {
+  if (goals.length === 0) {
+    return (
+      <InfoBox mode="hint">
+        <p>No goals found. Maybe add one?</p>
+      </InfoBox>
+    );
+  }
+
+  let warningBox: ReactNode;
+
+  if (goals.length >= 3) {
+    warningBox = (
+      <InfoBox mode="warning">
+        <p>Wow, that's a lot of goals! Consider removing some.</p>
+      </InfoBox>
+    );
+  }
+
   return (
-    <ul>
-      {goals.map((goal) => (
-        <li key={goal.id}>
-          <GoalApp title={goal.title} onDelete={() => onDeleteGoal(goal.id)}>
-            <p>{goal.description}</p>
-          </GoalApp>
-        </li>
-      ))}
-    </ul>
+    <>
+    {warningBox}
+      <ul>
+        {goals.map((goal) => (
+          <li key={goal.id}>
+            <GoalApp title={goal.title} onDelete={() => onDeleteGoal(goal.id)}>
+              <p>{goal.description}</p>
+            </GoalApp>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
